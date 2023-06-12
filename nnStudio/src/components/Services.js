266334dad84561cdf819a342d6ef46f63fe53a61
JakubStranianek <j.stranianek@gmail.com>
2023-06-12 19:46:25 +0200

    push services

--- nnStudio/src/components/Services.js
+++ nnStudio/src/components/Services.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const products = [
     {
@@ -8,7 +8,7 @@ const products = [
       name: 'Brazílsky keratín',
       href: 'brazilsky-keratin',
       price: 'od 35 €',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      imageName: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
       imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
     {
@@ -53,59 +53,65 @@ const products = [
     },
     {
       id: 7,
-      name: 'Bloombe',
-      href: 'bloombe',
+      name: 'Bomby do kúpeľa',
+      href: 'bomby-do-kupela',
       price: 'od 3.30 €',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
   ]
 
-export default function Services({data}) {   
-    
+export default function Services() {   
+    const data = useStaticQuery(graphql`
+      query {
+      allFile(filter: { relativeDirectory: { eq: "services" } }) {
+        nodes {
+          name
+          childImageSharp {
+            gatsbyImageData (
+              width: 280,
+              height: 320
+            )
+          }
+        }
+      }
+      }
+    `)
+
+    const images = data.allFile.nodes;
+
   return (
-    <div className="bg-white">        
+    <div className="bg-white"> 
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <p className="text-base font-semibold leading-7 text-green-800">Postaráme sa o vás</p>
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Ponuka naších služieb</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+          Svojou odbornosťou sa zameriavam na techniky, ako je brazilský keratín, 
+          jahodový a kokosový vlasový botox a hlboká rekonštrukcia vlasov. 
+          S precíznym detailom a láskou k vlasom, sa snažím priniesť vašim vlasom nový život, lesk a vitalitu.
+          </p>
+        </div>
+      </div>
+
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 
-            {data.Images}
-          {products.map((product) => (
-            
-            <a key={product.id} href={product.href} className="group">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => {
+            const imageFind = images.find((img) => img.name.includes(product.href));
+            return (
+              <a key={product.id} href={product.href} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">                
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
+                <GatsbyImage image={getImage(imageFind)} alt={product.imageAlt} className="h-full w-full object-cover object-center group-hover:opacity-75" />
               </div>
               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
             </a>
-          ))}
+           )
+          })}
         </div>
       </div>
     </div>
   )
 }
-
-export const query = graphql`
-query {
-    Images: allFile(filter: {relativeDirectory: {eq: "services"}}) {
-        edges {
-        node {
-            id
-            name
-            childImageSharp {
-                gatsbyImageData(
-                    width: 280
-                    placeholder: BLURRED
-                    formats: [AUTO, WEBP, AVIF]
-                )
-            }
-        }
-        }
-    }
-    }
-`
