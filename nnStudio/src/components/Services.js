0e07060e0d42103ebf7340fa0cafe883fcef7ebc
JakubStranianek <j.stranianek@gmail.com>
2023-06-12 19:53:33 +0200

    remove aspect

--- nnStudio/src/components/Services.js
+++ nnStudio/src/components/Services.js
@@ -102,7 +102,7 @@ export default function Services() {
             const imageFind = images.find((img) => img.name.includes(product.href));
             return (
               <a key={product.id} href={product.href} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">                
+              <div className="w-full overflow-hidden rounded-lg bg-gray-200">                
                 <GatsbyImage image={getImage(imageFind)} alt={product.imageAlt} className="h-full w-full object-cover object-center group-hover:opacity-75" />
               </div>
               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
